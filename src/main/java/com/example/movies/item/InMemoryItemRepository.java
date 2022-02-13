d902d85ecad7057d414260c36cf895b728041d16
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2022-02-13 00:34:34 +0000

    Added an InMemoryItemRepository interface with the KeyValue Dependency that provides all CRUD operations

---
+++ src/main/java/com/example/movies/item/InMemoryItemRepository.java
@@ -0,0 +1,7 @@
+package com.example.movies.item;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InMemoryItemRepository extends CrudRepository<Item, Long> {}
