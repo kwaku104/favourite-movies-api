09cab6a16668c0f8cf77ff12ddc1b505d926c557
Kwaku Appiah-Adu <kwakuappiahadu@gmail.com>
2022-04-18 18:53:42 +0100

    Added data validation to sanitize data

--- src/main/java/com/example/movies/item/Item.java
+++ src/main/java/com/example/movies/item/Item.java
@@ -1,12 +1,27 @@
 package com.example.movies.item;
 
+import org.hibernate.validator.constraints.URL;
 import org.springframework.data.annotation.Id;
+
+import javax.validation.constraints.*;
 
 public class  Item {
     private final Long id;
+
+    @NotNull(message="name is required")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "name must be a string")
     private final String name;
+
+    @NotNull(message="genre is required")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "genre must be a string")
     private final String genre;
+
+    @NotNull(message="description is required")
+    @Pattern(regexp="^[a-zA-Z ]+$", message = "description must be a string")
     private final String description;
+
+    @NotNull(message="image is required")
+    @URL(message = "image must be a URL")
     private final String image;
 
     public Item(Long id, String name, String genre, String description, String image) {
